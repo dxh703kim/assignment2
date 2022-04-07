5a26eea14a2b01aed35c968bed8b4167197ae4b4
dxh703kim <100106413+dxh703kim@users.noreply.github.com>
2022-04-06 23:45:13 -0400

    Update sketch.js

--- Day1/sketch.js
+++ Day1/sketch.js
@@ -4,33 +4,17 @@ function setup (){
 
     var i=0;
     var rand = random(1000);
-    var rand2 = random(50,1000); //random number between 50~1000
-    /*while(i<100){
-        fill(random(250));
-        // ellipse(50*i,50*i,250,250);
+    var rand2 = random(50,1000);
+    while(i<200){
+        noStroke();
+        fill(random(150));
         ellipse(random(1000),random(1000),250,250);
         i=i+1;
-    }*/
-
-    // fill(0);
-    // noStroke();
-    // rect(100,100,100,500);
-
-    // fill(0);
-    // stroke(0);
-    // ellipse(250,200,300,300);
-
-    // fill(87, 128, 91);
-    // stroke(0);
-    // ellipse(250,200,200,200); // ellipse(x,y,w,h)
+    }
 }
 
 function draw(){
-    noFill();
+    fill(255);
     stroke(0);
-    ellipse(mouseX,mouseY,random(100,250),random(100,250))
+    ellipse(mouseX,mouseY,150,150)
 }
-
-// function mousePressed(){
-//     saveCanvas("sketch-01","png")
-// }
