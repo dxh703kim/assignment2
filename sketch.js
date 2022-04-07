c5f32ba340b4387b6d6df6b593e0a7b114b47f9c
dxh703kim <100106413+dxh703kim@users.noreply.github.com>
2022-04-06 23:44:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup (){
+    createCanvas(1000,1000);
+    background(87, 128, 91);
+
+    var i=0;
+    var rand = random(1000);
+    var rand2 = random(50,1000); //random number between 50~1000
+    while(i<200){
+        noStroke();
+        fill(random(150));
+        // ellipse(50*i,50*i,250,250);
+        ellipse(random(1000),random(1000),250,250);
+        i=i+1;
+    }
+}
+
+function draw(){
+    fill(255);
+    stroke(0);
+    ellipse(mouseX,mouseY,150,150)
+}
+
+// function mousePressed(){
+//     saveCanvas("sketch-01","png")
+// }
